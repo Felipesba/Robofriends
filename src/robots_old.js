be9dba8cd5732107e85e446ee29939e516f773b7
felipesba <felipesba@gmail.com>
2018-06-29 00:20:48 -0300

    Cardlist feito

---
+++ src/robots_old.js
@@ -0,0 +1,28 @@
+export const robots = [
+	{
+		id: 1,
+		name: 'Felipe',
+		email: 'felipesba@gmail.com'
+	},
+	{
+		id: 2,
+		name: 'Thiago',
+		email: 'tsbda@gmail.com'
+	},
+	{
+		id: 3,
+		name: 'Bella',
+		email: 'bellacodiniz@gmail.com'
+	},
+	{
+		id: 4,
+		name: 'Eliane',
+		email: 'elisba@gmail.com'
+	},
+	{
+		id: 5,
+		name: 'Francisco',
+		email: 'fran@gmail.com'
+	},
+
+];
