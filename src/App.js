7a91df8c7343910222c24bf29f1807a5eda96e85
felipesba <felipesba@gmail.com>
2018-07-09 19:35:25 -0300

    RoboFriend quase pronto

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import React, {Component} from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import { robots } from './robots';
+// import { robots } from './robots';
+import './App.css';
+import Scroll from './Scroll'
 
 
 class App extends Component {
 	constructor(){
 		super();
 		this.state ={
-			robots: robots,
+			robots: [],
 			searchfield: ''
 		}
 	}
+
+componentDidMount() {
+	fetch('https://jsonplaceholder.typicode.com/users')
+		.then(response => response.json())
+		.then(users => this.setState({robots: users}));
+  }
 
 onSearchChange = (event) => {
 	this.setState({ searchfield: event.target.value })
@@ -23,13 +31,19 @@ render(){
 		return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
 	});
 
-	return	(
+	if (this.state.robots.length === 0) {
+		return(<h1>Loading...</h1>);
+	} else {
+		return	(
 		<div className='tc'>
-			<h1>Robofriend</h1>
+			<h1 className='f1'>Robofriend</h1>
 			<SearchBox searchChange={this.onSearchChange}/>
-			<CardList robots={filteredRobots}/>
+			<Scroll>
+				<CardList robots={filteredRobots}/>
+			</Scroll>
 		</div>
 	);	
+	}	
 }	
 
 }
